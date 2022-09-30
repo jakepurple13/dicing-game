1c20e632a539abff09d2d1991c7cd52d7e1958ea
Jacob Rein <>
2022-09-30 09:55:11 -0400

    Animating both the dice and the scores

--- app/src/main/java/viach/apps/dicing/ui/view/component/StatusBar.kt
+++ app/src/main/java/viach/apps/dicing/ui/view/component/StatusBar.kt
@@ -1,10 +1,11 @@
 package viach.apps.dicing.ui.view.component
 
+import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -12,7 +13,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 import viach.apps.dicing.dice.Dice
+import viach.apps.dicing.dicefactory.RandomDiceFactory
 
 @Composable
 fun StatusBar(
@@ -20,29 +23,39 @@ fun StatusBar(
     rightScore: Int,
     dice: Dice?
 ) {
+    var newDie by remember { mutableStateOf(dice) }
+
+    LaunchedEffect(dice) {
+        repeat(6) {
+            newDie = RandomDiceFactory.create()
+            delay(50)
+        }
+        newDie = dice
+    }
+
     Row(
         modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center
     ) {
         Text(
-            text = leftScore.toString(),
+            text = animateIntAsState(leftScore).value.toString(),
             modifier = Modifier.weight(1f),
             textAlign = TextAlign.Center,
             fontSize = 20.sp
         )
         Box(modifier = Modifier.size(100.dp)) {
-            if (dice != null) {
+            if (newDie != null) {
                 Icon(
-                    painter = painterResource(dice.iconRes),
-                    contentDescription = stringResource(dice.contentDescriptionRes),
+                    painter = painterResource(newDie!!.iconRes),
+                    contentDescription = stringResource(newDie!!.contentDescriptionRes),
                     modifier = Modifier.matchParentSize(),
                     tint = MaterialTheme.colors.primaryVariant
                 )
             }
         }
         Text(
-            text = rightScore.toString(),
+            text = animateIntAsState(rightScore).value.toString(),
             modifier = Modifier.weight(1f),
             textAlign = TextAlign.Center,
             fontSize = 20.sp
